f2eea4a6d90478bd315c445f324ca9d92acb1525
Maxime Martin <pro.maximemartin@gmail.com>
2021-05-22 13:41:59 +0200

    generate new Project

---
+++ forum/src/test/java/com/b2dev/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2dev.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- src/main/java/academy/campus/rest/entity/EnumRole.java
+++
@@ -1,5 +0,0 @@
-package academy.campus.rest.entity;
-
-public enum EnumRole {
-  ROLE_USER, ROLE_ADMIN
-}
